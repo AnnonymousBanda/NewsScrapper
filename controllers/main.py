33c6b577eec97ddc03281de74f972011a80c59e2
Ankit Bhagat <annonymous007banda@gmail.com>
2025-01-23 13:36:43 +0530

    feat: add the getTop controller

--- controllers/main.py
+++ controllers/main.py
@@ -2,13 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def getLatest(url: str = "https://indianexpress.com/latest-news/"):
+def getLatestNews(url: str = "https://indianexpress.com/latest-news/"):
     try:
         req = requests.get(url)
         req.raise_for_status()
         html = req.text
         soup = BeautifulSoup(html, "html.parser").find_all(class_="articles")
-        print(f"Number of articles found: {len(soup)}")
 
         data = []
         for k in soup:
@@ -39,4 +38,40 @@ def getLatest(url: str = "https://indianexpress.com/latest-news/"):
         return []
 
 
-getLatest()
+def getTopNews(url: str="https://indianexpress.com/top-news/"):
+    try:
+        req = requests.get(url)
+        req.raise_for_status()
+        html = req.text
+        soup = BeautifulSoup(html, "html.parser").find_all(class_="latest-articles")
+
+        data = []
+        for k in soup:
+            title = k.find(class_="latest-title")
+            article_url = k.find("a")["href"]
+            time = k.find(class_="latest-time")
+            image = k.find("img")
+            summary = k.find("p")
+
+            image_url = image["src"] if image else "No Image"
+            image_alt = image["alt"] if image else "No Alt Text"
+            
+            data.append(
+                {
+                    "title": title.get_text(strip=True) if title else "No Title",
+                    "url": article_url if article_url else "No URL",
+                    "time": time.get_text(strip=True) if time else "No Time",
+                    "image_url": image_url,
+                    "image_alt": image_alt,
+                    "summary": summary.get_text(strip=True) if summary else "No Summary",
+                }
+            )
+
+        return data
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred while fetching data: {e}")
+        return {}
+
+
+getTopNews()
