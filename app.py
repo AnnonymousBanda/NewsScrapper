4dbbc94f2dfd934a0e74c3c22e09226a903901f1
VinayKhedkar <arithmetic.chip@gmail.com>
2025-01-24 10:23:00 +0530

    fix: fix comments

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-# Description: This file contains the main code for the Flask app. It defines the routes and the functions to render the home, latest-news, and top-news pages. It also imports the getTopNews and getLatestNews functions from controllers/main.py to fetch the news data.
-# The home route renders the home page using a Jinja2 template. The latest-news and top-news routes fetch the news data using the getLatestNews and getTopNews functions, respectively, and render the corresponding pages using Jinja2 templates. In case of an exception, an error message is returned.
+# Description: This file contains the main code for the Flask app. It defines the routes and the functions to render the home, latest-news, and top-news pages. It also imports the `getTopNews` and `getLatestNews` functions from controllers/main.py to fetch the news data.
+# The home route renders the home page using a Jinja2 template. The latest-news and top-news routes fetch the news data using the `getLatestNews` and `getTopNews` functions, respectively, and render the corresponding pages using Jinja2 templates. In case of an exception, an error message is returned.
 
 # Importing required libraries
 from flask import Flask
 import requests
 from jinja2 import Environment, FileSystemLoader
+# Importing the getTopNews and getLatestNews functions from controllers/main.py to fetch the news data 
 from controllers.main import getTopNews, getLatestNews
 
 # Initializing the Flask app
@@ -16,7 +17,7 @@ app = Flask(__name__)
 # Function to render the home page
 def home():
     try:
-        # Rendering the home page using Jinja2 template
+        # Rendering the home page using Jinja2 template placed in the templates directory
         env = Environment(loader=FileSystemLoader("templates"))
         template = env.get_template("index.html")
         return template.render()
@@ -33,7 +34,7 @@ def latest():
     try:
         # Fetching the latest news using the getLatestNews function from controllers/main.py
         res = getLatestNews()
-        # Rendering the latest news page using Jinja2 template
+        # Rendering the latest news page using Jinja2 template placed in the templates directory
         env = Environment(loader=FileSystemLoader("templates"))
         template = env.get_template("latest.html")
         return template.render(news=res)
@@ -50,7 +51,7 @@ def top():
     try:
         # Fetching the top news using the getTopNews function from controllers/main
         res = getTopNews()
-        # Rendering the top news page using Jinja2 template
+        # Rendering the top news page using Jinja2 template placed in the templates directory
         env = Environment(loader=FileSystemLoader("templates"))
         template = env.get_template("top.html")
         return template.render(news=res)
